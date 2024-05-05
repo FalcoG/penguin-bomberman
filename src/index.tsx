271c40a0d677725e4914303578cdfd52159d928f
Falco Gerritsjans <alias.fcb319@falco.pro>
2024-05-05 23:01:15 +0200

    feat: add dark mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './lib/external/reasonable-colors.css';
+import './colors.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
