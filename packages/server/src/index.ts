7ac664fdd84b955412a7170193eb010c30d99371
Falco Gerritsjans <alias.fcb319@falco.pro>
2024-05-06 19:36:59 +0200

    feat: add server with deno

---
+++ packages/server/src/index.ts
@@ -0,0 +1,32 @@
+import { z } from 'https://deno.land/x/zod/mod.ts'
+
+const User = z.object({
+  username: z.string(),
+})
+
+console.log(User.parse({ username: 'Mario' }))
+
+Deno.serve((req) => {
+  if (req.headers.get('upgrade') != 'websocket') {
+    return new Response(null, { status: 501 })
+  }
+
+  const { socket, response } = Deno.upgradeWebSocket(req)
+
+  socket.addEventListener('open', () => {
+    console.log('a client connected!')
+  })
+
+  socket.addEventListener('close', () => {
+    console.log('a client disconnected!')
+  })
+
+  socket.addEventListener('message', (event) => {
+    console.log('client message', event.data)
+    if (event.data === 'ping') {
+      socket.send('pong')
+    }
+  })
+
+  return response
+})
