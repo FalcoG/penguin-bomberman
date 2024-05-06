652472f34ecb2cc10c0fab4c5f4ce80dabbe0545
Falco Gerritsjans <alias.fcb319@falco.pro>
2024-05-06 18:15:24 +0200

    feat: introduce pnpm workspaces

--- packages/client/lib/types.ts
+++ packages/client/lib/types.ts
@@ -4,4 +4,6 @@ export type TypeCoordinateY = number
 export type TPoint = { x: TypeCoordinateX; y: TypeCoordinateY }
 export type TPointArray = [TypeCoordinateX, TypeCoordinateY]
 
-export interface TGameState { grid: Array<'immutable_ice' | false> }
+export interface TGameState {
+  grid: Array<'immutable_ice' | false>
+}
